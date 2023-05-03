9e5a1d13875dfb39657621a6d9f8a02c7784930e
Sameer Ahmed <sameer7483@gmail.com>
2023-05-03 14:48:37 -0400

    Update infer.py

--- ngram-implementation-deployment/infer.py
+++ ngram-implementation-deployment/infer.py
@@ -23,4 +23,4 @@ def predict():
     return render_template('index.html', predicted_value = f'{prediction}')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, host = '0.0.0.0')
